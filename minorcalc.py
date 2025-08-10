625f33a64463962a4afb1d792e4fb5190c39e4fb
Devgoddy <godfreyddy513@gmail.com>
2025-08-10 02:32:10 +0000

    Create minorcalc.py

---
+++ minorcalc.py
@@ -0,0 +1,30 @@
+#Hello every one this is my first programing experience!!.
+
+
+num_1 = float(input( print ("Enter a valid input of choice:")))
+
+
+num_2 = float(input (print("Please provide your smart calc with option input two:")))
+
+#...remember simple operations first being kidies and not pros,,
+# just kiding
+
+#here we will add because every person needs more in life. 
+Res_Add = num_1 + num_2
+
+#here we will subtract because every person success needs more Sacrifice. 
+sub_sad = num_1 - num_2
+
+
+#here we will multiply because every person needs more repetions to be perfect in life. 
+pro_mult = num_1 * num_2
+
+#here we will divide because every person needs more in life. 
+Res_div = num_1 / num_2
+
+#now show the users that this is in control
+print(f"results of the two inputs: ")
+print(f"sum:{Res_Add}") 
+print(f"sub:{sub_sad}")
+print(f"mult:{pro_mult}")
+print(f"div:{Res_div}")   
